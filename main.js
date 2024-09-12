d9dd3c24e48e981b7bab1c4e8f4c0228ece175a1
tomeytran <tmtri310251@gmail.com>
2024-09-12 13:37:04 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// // Bài 1
+// let ly = prompt("Nhap diem Ly");
+// let hoa = prompt("Nhap diem Hoa");
+// let sinh = prompt("Nhap diem Sinh");
+// alert(
+// 	`Điểm trung bình của Lý Hoá Sinh = ${((ly + hoa + sinh) / 3).toFixed(2)}`
+// );
+
+// // Bài 2
+let doC = prompt("Nhap nhiet do Celsius");
+let doF = (doC * 9) / 5 + 32;
+alert(
+	`Theo công thức C/5 = (F-32)/9 thì nhiệt độ Fahrenheit tương ứng với ${doC} độ C bằng ${doF} độ F`
+);
